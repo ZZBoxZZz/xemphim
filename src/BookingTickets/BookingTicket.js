83ba9770d6292ebdc8cddaf0f2a2d6969a1afd1a
ZZBoxZZz <huydang1609.dpmh@gmail.com>
2023-01-09 22:50:45 +0700

    second update

--- src/BookingTickets/BookingTicket.js
+++ src/BookingTickets/BookingTicket.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 import './BaiTapBookingTicket.css';
 import ThongTinDatGhe from './ThongTinDatGhe.js'
+import danhSachGhe from '../data/danhSachGhe.json'
+import HangGhe from './HangGhe';
 export default class BookingTicket extends Component {
+
+
+  renderSeats = () => {
+    return danhSachGhe.map((hangGhe,index)=>{
+      return <div key ={index} >
+        <HangGhe hangGhe={hangGhe} HangDau = {index} />
+      </div>
+    })
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   render() {
     return (
       <div className="bookingMovive" style={{position: "fixed", width: "100%", height: "100%", backgroundImage: "url('./img/bgmovie.jpg')", backgroundSize:"100%"}}>
@@ -16,10 +43,12 @@ export default class BookingTicket extends Component {
                     <div className='col-8 text-center'>
                         <div className='text-light display-4'>Đặt vé xem phim tai đây bla bal</div>
                         <div className='mt-5 text-light'style={{fontSize: '15px'}}>Màn hình</div>
-                        <div className='mt-1' style={{display: "flex", flexDirection:'row', justifyContent: 'center'}}>
+                        <div className='mt-1' style={{display: "flex", flexDirection:'column', justifyContent: 'center'}}>
                           
                           <div className='screen'></div>
+                          {this.renderSeats()}
                         </div>
+                        
                         
                     </div>
                 </div>
--- src/BookingTickets/HangGhe.js
+++ src/BookingTickets/HangGhe.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
 
 export default class HangGhe extends Component {
+
+
+  renderGhe = () => {
+    return this.props.hangGhe.danhSachGhe.map((ghe,index) =>{
+      // if(index===0){
+      //   return <span className="rowNumber">{ghe.soGhe}</span>
+      // }
+      let cssGheDaDat = '';
+      let disabled = false;
+
+      if(ghe.daDat){
+        cssGheDaDat='gheDuocChon';
+      }
+      return <button disabled ={disabled} className = {`ghe ${cssGheDaDat}`} key ={index}>
+        {index+1}
+
+      </button>
+    })
+  }
+
+  renderSoHang = () =>{
+    return this.props.hangGhe.danhSachGhe.map((hang, index) =>{
+      return <button className="rowNumber">{hang.soGhe}</button>
+  })}
+  renderHangGhe =() => {
+    if(this.props.HangDau ===0){
+      return <div className='ml-4'>
+        {this.props.hangGhe.hang} {this.renderSoHang()}
+      </div>
+    } else{
+      return <div>
+        {this.props.hangGhe.hang} {this.renderGhe()}
+
+      </div>
+    }
+    
+  }
+
   render() {
     return (
-      <div>
-        
+      <div className='text-light text-left ml-5 mt-5' style={{fontSize:30}}>
+        {this.renderHangGhe()}
       </div>
     )
   }
